f48552150afb95ec7e9419e9a75c19ef6a9fd946
Jakub Lisowski <lisowski.jakub@gmail.com>
2023-05-28 18:22:04 +0200

    fixed vite config.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'react-hw1-template',
+  base: '/react-hw1-template',
   plugins: [react()],
 })
